8e19b24d325838bee5bd7e729d26931a9d255e49
Evilmove <100401459+Evilmove@users.noreply.github.com>
2023-12-14 12:56:01 +0530

    initial commit

---
+++ init/init.oplus.sh
@@ -0,0 +1,38 @@
+#! /vendor/bin/sh
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+prjname=`getprop ro.boot.prjname`
+
+# Radio
+if [ "$prjname" = "19706" ] || [ "$prjname" = "19795" ] || [ "$prjname" = "20607" ]; then
+    setprop vendor.radio.multisim.config dsds
+elif [ "$prjname" = "19705" ]; then
+    setprop vendor.radio.multisim.config ssss
+fi
+
+# Wifi
+persistbdf=`md5sum /mnt/vendor/persist/bdwlan.elf |cut -d" " -f1`
+persistRegdb=`md5sum /odm/etc/wifi/regdb.bin |cut -d" " -f1`
+vendorbdf=`md5sum /odm/etc/wifi/bdwlan.elf |cut -d" " -f1`
+vendorRegdb=`md5sum /mnt/vendor/persist/regdb.bin |cut -d" " -f1`
+
+if [ x"$vendorbdf" != x"$persistbdf" ]; then
+    cp /odm/etc/wifi/bdwlan.elf /mnt/vendor/persist/bdwlan.elf
+    sync
+    echo "bdf check"
+fi
+
+if [ x"$vendorRegdb" != x"$persistRegdb" ]; then
+    cp /odm/etc/wifi/regdb.bin /mnt/vendor/persist/regdb.bin
+    sync
+    echo "regdb check"
+fi
+
+chmod 666 /mnt/vendor/persist/bdwlan.elf
+chmod 666 /mnt/vendor/persist/regdb.bin
+chown system:wifi /mnt/vendor/persist/bdwlan.elf
+chown system:wifi /mnt/vendor/persist/regdb.bin
---
+++ libinit/init_realme_kona.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+#include <cstdlib>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+#include "property_service.h"
+
+using android::base::GetProperty;
+using std::string;
+
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
+
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+}
+
+static const variant_info_t RMX2071_CN_info = {
+    .prjversion_value = "19795",
+
+    .device = "RMX2071CN",
+    .model = "RMX2071",
+    .name = "RMX2071",
+    .sku = "bladerunner",
+};
+
+static const variant_info_t RMX2072_CN_info = {
+    .prjversion_value = "20607",
+
+    .device = "RMX2072CN",
+    .model = "RMX2072",
+    .name = "RMX2072",
+    .sku = "bladerunner_48m",
+};
+
+static const variant_info_t RMX2075_GLOBAL_info = {
+    .prjversion_value = "19705",
+
+    .device = "RMX2075L1",
+    .model = "RMX2075",
+    .name = "RMX2075",
+    .sku = "bladerunner",
+};
+
+static const variant_info_t RMX2076_IN_info = {
+    .prjversion_value = "19706",
+
+    .device = "RMX2076L1",
+    .model = "RMX2076",
+    .name = "RMX2076",
+    .sku = "bladerunner",
+};
+
+static const variant_info_t RMX3370_CN_info = {
+    .prjversion_value = "21619",
+
+    .device = "RE5473",
+    .model = "RMX3370",
+    .name = "RMX3370",
+};
+
+static const variant_info_t RMX3370_CNLZ_info = {
+    .prjversion_value = "136730",
+
+    .device = "RE5473",
+    .model = "RMX3370",
+    .name = "RMX3370",
+};
+
+static const variant_info_t RMX3370_GLOBAL_info = {
+    .prjversion_value = "136859",
+
+    .device = "RE879AL1",
+    .model = "RMX3370",
+    .name = "RMX3370",
+};
+
+static const variant_info_t RMX3370_IN_info = {
+    .prjversion_value = "136858",
+
+    .device = "RE879AL1",
+    .model = "RMX3370",
+    .name = "RMX3370",
+};
+
+static const std::vector<variant_info_t> variants = {
+    RMX2071_CN_info,
+    RMX2072_CN_info,
+    RMX2075_GLOBAL_info,
+    RMX2076_IN_info,
+    RMX3370_CN_info,
+    RMX3370_CNLZ_info,
+    RMX3370_GLOBAL_info,
+    RMX3370_IN_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    
+    // Misc
+    property_override("ro.matrixx.chipset", "Snapdragon® 865 5G");
+    property_override("ro.matrixx.device", "Realme X50 Pro");
+    property_override("ro.matrixx.maintainer", "AAMIRR ALI");
+}
